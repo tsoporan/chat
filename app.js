7d9f777276ca121d4ab1960d57ba57353b146fde
Titus Soporan <titus@tsoporan.com>
2014-10-11 20:12:16 -0400

    re-arranging for angular dir structure

--- app.js
+++ app.js
@@ -42,7 +42,6 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    console.log('in here');
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
