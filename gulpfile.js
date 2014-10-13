681fed99a05e51bc9f6c5a13420a9595d56ffe59
Titus Soporan <titus@tsoporan.com>
2014-10-13 18:16:32 -0400

    new gulp build, utilizing bower

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,25 @@
-var gulp   = require('gulp'),
-    clean  = require('gulp-clean'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    rename = require('gulp-rename'),
-    gutil  = require('gulp-util'),
-    less   = require('gulp-less'),
-    minify = require('gulp-minify-css'),
-    hint   = require('gulp-jshint'),
-    rev    = require('gulp-rev');
+// Building with Gulp.
+
+var gulp       = require('gulp'),
+    del        = require('del'),
+    bowerFiles = require('main-bower-files'),
+    uglify     = require('gulp-uglify'),
+    concat     = require('gulp-concat'),
+    rename     = require('gulp-rename'),
+    less       = require('gulp-less'),
+    minify     = require('gulp-minify-css'),
+    hint       = require('gulp-jshint'),
+    rev        = require('gulp-rev');
 
 gulp.task('clean', function() {
-    return gulp.src(['public/build',], {read: false})
-    .pipe(clean());
+    return del([
+      'public/build/*',
+    ]);
+});
 
+gulp.task('collect-libs', function() {
+  return gulp.src(bowerFiles())
+         .pipe(gulp.dest('public/build/libs'));
 });
 
 gulp.task('less', function() {
@@ -21,8 +28,8 @@ gulp.task('less', function() {
   .pipe(gulp.dest('public/stylesheets/'));
 });
 
-gulp.task('css', function() {
-  return gulp.src(['public/stylesheets/vendor/**/*.css', 'public/stylesheets/*.css'])
+gulp.task('build-css', ['less'], function() {
+  return gulp.src(['public/build/libs/*.css', 'public/stylesheets/*.css'])
   .pipe(minify())
   .pipe(concat('build.min.css'))
   .pipe(gulp.dest('public/build'));
@@ -34,25 +41,25 @@ gulp.task('lint', function() {
   .pipe(hint.reporter('default'));
 });
 
-gulp.task('js', function() {
-    return gulp.src(['public/javascripts/vendor/jquery.min.js',
-                     'public/javascripts/vendor/jquery-ui.min.js',
-                     'public/javascripts/vendor/fastclick.js',
-                     'public/javascripts/vendor/modernizr.js',
-                     'public/javascripts/vendor/foundation.min.js',
-                     'public/javascripts/vendor/moment.js',
-                     'public/javascripts/vendor/placeholder.js',
-                     'public/javascripts/vendor/socket.io.js',
-                     'public/javascripts/chat.js'
+gulp.task('copy-socketio', function() {
+  return gulp.src('bower_components/socket.io-client/socket.io.js')
+         .pipe(gulp.dest('public/build/libs'));
+});
+
+gulp.task('build-js', ['copy-socketio', 'lint'], function() {
+
+    return gulp.src(['public/build/libs/socket.io.js',
+                     'public/build/libs/moment.js',
+                     'public/build/libs/angular.js',
+                     'public/javascripts/chat.js' // App
     ])
     .pipe(concat('build.js'))
     .pipe(uglify())
     .pipe(rename('build.min.js'))
-    .pipe(gulp.dest('public/build'))
-    .on('error', gutil.log);
+    .pipe(gulp.dest('public/build'));
 });
 
-gulp.task('rev', ['css', 'js'], function() {
+gulp.task('rev', ['build-css', 'build-js'], function() {
   return gulp.src(['public/build/*.js', 'public/build/*.css'], {base: 'public/build'})
   .pipe(gulp.dest('public/build'))
   .pipe(rev())
@@ -61,4 +68,4 @@ gulp.task('rev', ['css', 'js'], function() {
   .pipe(gulp.dest('public/build'));
 });
 
-gulp.task('default', ['clean', 'less', 'css', 'lint', 'js', 'rev']);
+gulp.task('default', ['clean', 'collect-libs', 'build-css', 'build-js', 'rev']);
