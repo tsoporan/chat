eab087cb2151ff8fdce2e8e3e18925ad6fc65adb
Titus Soporan <titus@tsoporan.com>
2014-10-20 20:57:12 -0400

    remove asset injection from express

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,8 @@ var router = express.Router();
 var fs = require('fs');
 
 router.get('/', function(req, res) {
-
-  var manifest  = JSON.parse(fs.readFileSync('public/build/rev-manifest.json')),
-      assetPath = '/build/';
-
   res.render('index', {
     title          : '#chat',
-    stylesheetPath : assetPath + manifest['build.min.css'],
-    javascriptPath : assetPath + manifest['build.min.js'],
   });
 });
 
