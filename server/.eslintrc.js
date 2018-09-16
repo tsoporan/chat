a70a642f2ecb21bc205b0503dc6735395a967c82
Titus Soporan <titus@tsoporan.com>
2018-09-15 20:09:11 -0400

    Added eslint for server
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

---
+++ server/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: false,
+    es6: true
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"]
+  }
+};
