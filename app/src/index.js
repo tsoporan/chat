784441558d604770d1c826ad404f7d495152d407
Titus Soporan <titus@tsoporan.com>
2018-09-15 22:19:08 -0400

    Basic layout using semantic ui
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
+
+import "semantic-ui-css/semantic.min.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
