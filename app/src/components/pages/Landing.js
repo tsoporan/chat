f285017389572501857f721ff2ddf4e3187936b5
Titus Soporan <titus@tsoporan.com>
2018-10-09 21:08:41 -0400

    Update landing page structure
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- app/src/components/pages/Landing.js
+++ app/src/components/pages/Landing.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
+
+import { Header, List } from "semantic-ui-react";
+
+const popularNetworks = ["freenode", "efnet", "hashbang"];
 
 function Landing() {
-  return <div>Landing</div>;
+  const listItems = popularNetworks.map((network, idx) => (
+    <List.Item key={idx}>{network}</List.Item>
+  ));
+
+  return (
+    <Fragment>
+      <Header as="h2" textAlign="center">
+        <Header.Content>Let's chat! Connect to get started.</Header.Content>
+        <Header.Subheader>
+          First time? Choose from a popular network below.
+        </Header.Subheader>
+      </Header>
+      <List>{listItems}</List>
+    </Fragment>
+  );
 }
 
 export default Landing;
