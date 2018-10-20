7ca011bc75d6328e66b5986a67bda371002cad9c
Titus Soporan <titus@tsoporan.com>
2018-10-20 18:23:30 -0400

    Make app root 100% height
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- app/src/index.js
+++ app/src/index.js
@@ -5,6 +5,7 @@ import registerServiceWorker from "./registerServiceWorker";
 import App from "./components";
 
 import "semantic-ui-css/semantic.min.css";
+import "./styles/index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
